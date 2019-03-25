a99d97488123f3452982afd4c3e2a30681d6f349
Junghwa Jung <wjdghk516@naver.com>
2019-03-25 17:12:29 +0900

    Chapter10

---
+++ ch10/src/exercise/WrongPasswordException.java
@@ -0,0 +1,8 @@
+package exercise;
+
+public class WrongPasswordException extends Exception {
+	public WrongPasswordException() {}
+	public WrongPasswordException(String message) {
+		super(message);
+	}
+}
