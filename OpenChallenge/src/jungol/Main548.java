4a15a8bde1f7e9e2ddd238ea8f6258c983841a07
Junghwa Jung <wjdghk516@naver.com>
2019-04-01 17:45:49 +0900

    OpenChallenge

---
+++ OpenChallenge/src/jungol/Main548.java
@@ -0,0 +1,14 @@
+package jungol;
+
+
+public class Main548 {
+
+	public static void main(String[] args) {
+		for(int i=2; i<5; i++) {
+			for(int k=1; k<=5; k++) {
+				System.out.print(i + " * " + k + " = " + (i*k) + "   ");
+			}
+			System.out.println();
+		}
+	}
+}
