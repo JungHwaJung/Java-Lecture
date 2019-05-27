e71fda8f29e29cb411147fe45c6387e647a3d448
Junghwa Jung <wjdghk516@naver.com>
2019-05-27 16:20:55 +0900

    GUI

---
+++ GUI/src/ComboActionEx.java
@@ -0,0 +1,43 @@
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ComboActionEx extends JFrame {
+	Container contentPane;
+	String [] fruits = {"apple", "cherry", "kiwi", "mango"};
+	ImageIcon [] images = {
+			new ImageIcon("images/apple.png"),
+			new ImageIcon("images/cherry.jpg"),
+			new ImageIcon("images/kiwi.jpg"),
+			new ImageIcon("images/mango.jpg") 
+	};
+	JLabel imgLabel = new JLabel(images[0]);
+	
+	ComboActionEx() {
+		setTitle("리스트 만들기 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane = getContentPane();
+		contentPane.setLayout(new FlowLayout());
+		
+		JComboBox strCombo = new JComboBox(fruits);
+		strCombo.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JComboBox cb = (JComboBox)e.getSource();
+				int index = cb.getSelectedIndex();
+				imgLabel.setIcon(images[index]);
+			}
+		});
+		contentPane.add(strCombo);
+		contentPane.add(imgLabel);
+		setSize(200, 300);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new ComboActionEx();
+	}
+
+}
